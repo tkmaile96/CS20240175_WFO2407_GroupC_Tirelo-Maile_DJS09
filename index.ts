278fcd7d61b1d3d76623dcb3f6ed45c2ca2f0e48
Teekay <tkmaile96@gmail.com>
2024-11-21 03:28:35 +0200

    Added new user object and populateUser function, and called it with the new user data

--- index.ts
+++ index.ts
@@ -31,3 +31,16 @@ function showReviewTotal (value : number, reviewer : string, isLoyalty : boolean
 }
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+const you = {
+    userName: 'Teekay';
+    isReturning: true;
+}
+
+function populateUser(isReturning : boolean, userName : string) {
+    if(isReturning) {
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
+populateUser(you.isReturning, you.userName)
